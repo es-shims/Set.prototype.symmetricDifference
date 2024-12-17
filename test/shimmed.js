5670bb18a82043ba9946a6d186f33e6136c4f8c2
Jordan Harband <ljharb@gmail.com>
2024-12-17 13:34:53 -0800

    [Tests] add shim status

--- test/shimmed.js
+++ test/shimmed.js
@@ -1,12 +1,17 @@
 'use strict';
 
+var orig = typeof Set === 'function' && Set.prototype.symmetricDifference;
+
 require('../auto');
+
+var shimmed = orig !== Set.prototype.symmetricDifference;
 
 var test = require('tape');
 
 var runTests = require('./builtin');
 
 test('shimmed', function (t) {
+	t.comment('shimmed: ' + (shimmed ? 'YES' : 'NO'));
 	runTests(t);
 
 	t.end();
